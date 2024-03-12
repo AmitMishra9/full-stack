07214e98a654b697d7c45245db493513649a17e8
Amit <amitmishra10111@gmail.com>
2024-03-12 13:21:01 +0530

    nav done

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,78 +2,69 @@ import { Navigate, useNavigate } from "react-router";
 import "./Header.scss";
 import { Layout, Menu } from "antd";
 import { useEffect, useState } from "react";
-//import { useState } from "react";
+
 const { Header: Navbar } = Layout;
 
 const Header = () => {
   const navigate = useNavigate();
-  const[islogedin,setislogedin]=useState(false);
-  
-  useEffect(()=>{
-    if(localStorage.getItem('jwt')){
-       setislogedin(true);
-    }
-    else{
+  const [islogedin, setislogedin] = useState(false);
+  const [reload,setreload]=useState(false);
+  useEffect(() => {
+    if (localStorage.getItem('jwt')) {
+      setislogedin(true);
+    } else {
       setislogedin(false);
     }
-  },[])
+  }, [reload]);
 
-
- //const islogedin= localStorage.getItem('jwt');
-  //console.log(islogedin);
- const menuItem=islogedin?[
-   
-    {
-         key:"/",
-         label:"Logout"
-    },
-    {
-        key:"/home",
-        label:"Home"
-   },
-   {
-    key:"/create-blog",
-    label:"Create Blog",
-   }
-
- ] : [
-  {
-    key:"/",
-    label:"Login"
-},
-{
-   key:"/home",
-   label:"Home"
-},
-{
-key:"/create-blog",
-label:"Create Blog",
-}
-
- ];
-
- 
-  const onMenuClick = (e) => {
-    console.log(e.key);
-  if (e.key === "/login") {
-    setislogedin(false);
-   
-    navigate("/login"); // Redirect to the login page or any other appropriate route
-  } else {
-    navigate(e.key);
-  }
-    navigate(e.key);
+  const updateLoginStatus = (isLoggedIn) => {
+    setislogedin(isLoggedIn);
   };
 
+  const [menuItem, setMenuitem] = useState(islogedin
+    ? 
+    [
+      {
+        key: "/",
+        label: "Login"
+      },
+      {
+        key: "/home",
+        label: "Home"
+      },
+      {
+        key: "/create-blog",
+        label: "Create Blog",
+      }
+    ]:[
+      {
+        key: "/",
+        label: "Logout"
+      },
+      {
+        key: "/home",
+        label: "Home"
+      },
+      {
+        key: "/create-blog",
+        label: "Create Blog",
+      }
+    ]
+  );
 
-
-
-
-
-
-
-
-
+  const onMenuClick = (e) => {
+    console.log(e.key);
+    if (e.key === "/") {
+      setislogedin(false);
+      localStorage.removeItem("jwt");
+      updateLoginStatus(false); // Update the state to trigger re-render
+      setMenuitem(menuItem);
+      setreload(!reload);
+      navigate("/"); // Redirect to the login page or any other appropriate route
+    } else {
+      navigate(e.key);
+    }
+  };
 
   return (
     <header className="blog-hearder">
@@ -93,5 +84,3 @@ label:"Create Blog",
 };
 
 export default Header;
-
-//40:45
